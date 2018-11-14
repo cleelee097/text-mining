8ec989d80744292e5ffba6ef2a01a6f4cdc9205a
Christine Lee <clee7@babson.edu>
2018-11-14 12:54:54 -0500

    script for 2nd assignment

---
+++ text-mining.py
@@ -0,0 +1,76 @@
+import re 
+import tweepy 
+from textblob import TextBlob 
+from tweepy import OAuthHandler 
+from twython import Twython
+
+  
+class TwitterClient(object): 
+    def __init__(self): 
+        token = '925843099947020289-jJVNhc6E422ibriXuUKsyQ9ZLElqu7B'
+        token_secret = 'Z27naC6N0x7yuUMvbkHPwrqngmYwRjAH7gHUNw2CPBPnw'
+        consumer_key = 'lCxcMZBe12D390ojuVZGOTtT1'
+        consumer_secret = 'pdMVg9kvE4f0pfEDQu41WiGKnFSq4wMnxn15RNr8R1zTqonm0S'
+         
+        try:  
+            self.auth = OAuthHandler(consumer_key, consumer_secret) 
+            self.auth.set_access_token(token, token_secret) 
+            self.api = tweepy.API(self.auth) 
+        except: 
+            print("Error: Authentication Failed") 
+
+    def clean_tweet(self, tweet): 
+        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])(\w+:\/\/\S+)", " ", tweet).split())
+
+    def get_tweet_sentiment(self, tweet): 
+        analysis = TextBlob(self.clean_tweet(tweet)) 
+        if analysis.sentiment.polarity > 0: 
+            return 'positive'
+        elif analysis.sentiment.polarity == 0: 
+            return 'neutral'
+        else: 
+            return 'negative'
+  
+    def get_tweets(self, query, count = 10): 
+        tweets = [] 
+        try: 
+            fetched_tweets = self.api.search(q = query, count = count) 
+  
+            for tweet in fetched_tweets: 
+                parsed_tweet = {} 
+   
+                parsed_tweet['text'] = tweet.text  
+                parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet.text) 
+  
+                if tweet.retweet_count > 0: 
+                    if parsed_tweet not in tweets: 
+                        tweets.append(parsed_tweet) 
+                else: 
+                    tweets.append(parsed_tweet) 
+  
+            return tweets 
+  
+        except tweepy.TweepError as e:  
+            print("Error : " + str(e)) 
+  
+def main(): 
+    api = TwitterClient()  
+    tweets = api.get_tweets(query = '#Celtics', count = 100) 
+  
+    ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive'] 
+    print("Positive tweets percentage: {} %".format(100*len(ptweets)/len(tweets)))  
+    ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative'] 
+    print("Negative tweets percentage: {} %".format(100*len(ntweets)/len(tweets))) 
+  
+    # printing first 5 positive tweets 
+    print("\n\nPositive tweets:") 
+    for tweet in ptweets[:10]: 
+        print(tweet['text']) 
+  
+    # printing first 5 negative tweets 
+    print("\n\nNegative tweets:") 
+    for tweet in ntweets[:10]: 
+        print(tweet['text']) 
+  
+if __name__ == "__main__": 
+    main()
